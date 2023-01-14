b83aeaa0d1d7db5c4b3ed71a6f091268b1c7d3a5
Adriana Ruiz <adriannaruiz24@gmail.com>
2023-01-13 19:26:10 -0500

    con responsive listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TodoCreate from './components/TodoCreate'
 import TodoFilter from './components/TodoFilter'
 import TodoList from './components/TodoList'
 
-const initialState = [
-  {id: 1, title: 'study english', completed: true},
-  {id: 2, title: 'study react', completed: false},
-  {id: 3, title: 'study MySQL', completed: true},
-  {id: 4, title: 'app', completed: false},
-]
+// const initialState = [
+//   {id: 1, title: 'study english', completed: true},
+//   {id: 2, title: 'study react', completed: false},
+//   {id: 3, title: 'study MySQL', completed: true},
+//   {id: 4, title: 'app', completed: false},
+// ]
+
+const initialState = JSON.parse(localStorage.getItem("todo")) || [];
 
 const App = () => {
 
   const [todo, setTodo] = useState(initialState)
   const [option, setOption] = useState('all')
+
+  useEffect(() => {
+    localStorage.setItem('todo', JSON.stringify(todo))
+  }, [todo])
 
   const createTodo = (title) => {
     const newTodo = {
@@ -58,11 +64,11 @@ const App = () => {
   
 
   return (
-    <div className='min-h-screen bg-gray-200 bg-[url("./assets/images/bg-mobile-light.jpg")] bg-no-repeat bg-contain dark:bg-gray-900 dark:bg-[url("./assets/images/bg-mobile-dark.jpg")] transition-all duration-1000'>
+    <div className='min-h-screen bg-gray-200 bg-[url("./assets/images/bg-mobile-light.jpg")] bg-no-repeat bg-contain dark:bg-gray-900 dark:bg-[url("./assets/images/bg-mobile-dark.jpg")] transition-all duration-1000 md:bg-[url("./assets/images/bg-desktop-light.jpg")] md:dark:bg-[url("./assets/images/bg-desktop-dark.jpg")]'>
       
       <Header/>
 
-      <main className='container mx-auto px-4 mt-6'>
+      <main className='container mx-auto px-4 mt-6 md:max-w-xl'>
         <TodoCreate createTodo={createTodo}/>
        
         {/* TodoList  (TodoItem) TodoUpdate & TodoDelete*/}
